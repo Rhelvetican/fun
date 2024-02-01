ee216c9559bb1ab2740a9b1999c853c17cfa82d8
Rh4096 <rhelvetican@gmail.com>
2024-02-01 20:19:55 +0700

    error

--- src/error.rs
+++ src/error.rs
@@ -1,1 +1,26 @@
+#[derive(Debug)]
+pub enum ProgramErr {
+    IoErr(std::io::Error),
+    ClapErr(clap::Error),
+    SerdeJsonErr(serde_json::Error),
+    ExitErr(String),
+    FirstRunPromptCancelErr(String),
+}
 
+impl From<std::io::Error> for ProgramErr {
+    fn from(e: std::io::Error) -> Self {
+        Self::IoErr(e)
+    }
+}
+
+impl From<clap::Error> for ProgramErr {
+    fn from(e: clap::Error) -> Self {
+        Self::ClapErr(e)
+    }
+}
+
+impl From<serde_json::Error> for ProgramErr {
+    fn from(e: serde_json::Error) -> Self {
+        Self::SerdeJsonErr(e)
+    }
+}
